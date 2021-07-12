575d80462c3868f7fdf8c71639eee5967f420574
Alena Litouka <alena.litouka@gmail.com>
2021-07-12 10:15:45 -0400

    identifiers and operators

--- src/main/java/TestClass.java
+++ src/main/java/TestClass.java
@@ -1,10 +1,5 @@
 public class TestClass {
     public static void main (String[] args) {
-       // System.out.println("Hello World");
-        float value1 = 1f * (boolean)0;
-        System.out.println(value1);
-        /*
-         * /* ferret */
-*/
+        System.out.println("Hello World");
     }
 }
