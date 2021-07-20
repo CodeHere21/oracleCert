88171e33c077c4a4dae28767bf57940bca73250e
Alena Litouka <alena.litouka@gmail.com>
2021-07-19 21:50:26 -0400

    3345

--- src/main/java/TestClass.java
+++ src/main/java/TestClass.java
@@ -1,12 +1,14 @@
 public class TestClass {
     public static void main (String[] args) {
        // System.out.println("Hello World");
-        int x=0;
-        while ( ++x < 5 )
-        {
-            x+=1;
-        }
-        String message = x > 5 ? "Greater than" : "Less Than";
-        System.out.println(message+","+x);
+//        int x=0;
+//        while ( ++x < 5 )
+//        {
+//            x+=1;
+//        }
+//        String message = x > 5 ? "Greater than" : "Less Than";
+//        System.out.println(message+","+x);
+
+        System.out.println(1+2+"3"+4+5);
     }
 }
