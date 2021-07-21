2b5b9f5b2240a4ab99381f3f6c0c6e5a2ab3a45f
Alena Litouka <alena.litouka@gmail.com>
2021-07-21 15:59:54 -0400

    nested loops

--- src/main/java/TestClass.java
+++ src/main/java/TestClass.java
@@ -10,5 +10,31 @@ public class TestClass {
 //        System.out.println(message+","+x);
 
   //      System.out.println(1+2+"3"+4+5);
+//        java.util.List<String> values = new java.util.ArrayList<String>();
+//        values.add("Lisa");
+//        values.add("Kevin");
+//        values.add("Roger");
+//        for(String value : values) {
+//            System.out.print(value + ", ");
+//        }
+//        java.util.List<String> names = new java.util.ArrayList<String>();
+//        names.add("Lisa");
+//        names.add("Kevin");
+//        names.add("Roger");
+//        for(int i=0; i<names.size(); i++) {
+//            String name = names.get(i);
+//            if(i>0) {
+//                System.out.print(", ");
+//            }
+//            System.out.print(name);
+//        }
+        int x=20;
+        while(x>0){
+            do{
+                x-=2;
+            } while (x>5);
+            x--;
+            System.out.print(x+"\t");
+        }
     }
 }
