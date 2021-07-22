df4e34bc9e4ff67d450be206eabbf7ce881d08f9
Alena Litouka <alena.litouka@gmail.com>
2021-07-22 18:21:17 -0400

    concat, equals

--- src/main/java/TestClass.java
+++ src/main/java/TestClass.java
@@ -74,6 +74,15 @@ public class TestClass {
 //        sb.insert(0,"-");
 //        sb.insert(4,"-");
 //        System.out.println(sb);
-        System.out.println(LocalDate.now());
+        //System.out.println(LocalDate.now());
+
+//String s="Hello";
+//String t=new String (s);
+//if("Hello".equals(s)) System.out.println("one");
+//if(t==s) System.out.println("two");
+//if(t.equals(s)) System.out.println("three");
+//if("Hello"==s) System.out.println("four");
+//if("Hello"==t) System.out.println("five");
+
     }
 }
